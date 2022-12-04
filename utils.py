bc948184399626d6d925d563bebde7af273e7055
Noel Rajan <leonr13@hotmail.co.uk>
2022-12-04 16:49:40 +0000

    utils script

---
+++ utils.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+
+import requests
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+session_token = config["AOC_TOKEN"]
+
+
+def get_input(day, year):
+    puzzle_input = Path(f"./{year}/{day}/{day}.txt")
+    print(f"Input Data: {puzzle_input}")
+
+    if puzzle_input.is_file():
+        return Path(puzzle_input).read_text(encoding="utf-8").strip()
+
+    url = f"https://adventofcode.com/{year}/{day}/{day}/input"
+    headers = {"Cookie": "session=" + session_token}
+    req = requests.get(url, headers=headers, timeout=5)
+    if req.status_code == 200:
+        puzzle_input.write_text(req.text, encoding="utf-8")
+        return req.text
+    else:
+        sys.exit(
+            f"/api/alerts response: {req.status_code}: {req.reason} \n{req.content}"
+        )
