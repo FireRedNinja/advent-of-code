7d896698be4be8d0acab4fe6a511ff5073c4b249
Noel Rajan <leonr13@hotmail.co.uk>
2022-12-04 16:51:24 +0000

    update template script

--- 0/0.py
+++ 0/0.py
@@ -1,44 +1,29 @@
-import requests
-from pathlib import Path
-import sys
+from utils import get_input
 
-from dotenv import dotenv_values
+# TODO
+# - add testing
 
-config = dotenv_values('.env')
-session_token = config["AOC_TOKEN"]
-
-def get_input(day, year):
-    puzzle_input = Path(f"./{year}/{day}.txt")
-    print(f"Input Data: {puzzle_input}")
-
-    if puzzle_input.is_file():
-        return Path(puzzle_input).read_text().strip()
-    
-    url = "https://adventofcode.com/2021/day/"+str(day)+"/input"
-    headers = {'Cookie': 'session='+session_token}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        puzzle_input.write_text(r.text)
-        return r.text
-    else:
-        sys.exit(f"/api/alerts response: {r.status_code}: {r.reason} \n{r.content}")
+DAY = "0"
+YEAR = "2022"
+P1_TEST_DATA = ""
+P2_TEST_DATA = ""
 
 
-def parse(puzzle_input):
+def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return [line for line in puzzle_input.splitlines()]
 
 
 def part1(data):
     """Solve part 1"""
 
-    return f'Not implemented'
+    return f"Not implemented"
 
 
 def part2(data):
     """Solve part 2"""
 
-    return f'Not implemented'
+    return f"Not implemented"
 
 
 def solve(puzzle_input):
@@ -51,11 +36,8 @@ def solve(puzzle_input):
 
 
 if __name__ == "__main__":
-    day = '0'
-    year = "2022"
-
-    puzzle_input = get_input(day, year)
+    puzzle_input = get_input(DAY, YEAR)
 
     solutions = solve(puzzle_input)
-    print('\nSolutions:')
-    print(f'\tPart 1: {solutions[0]}\n\tPart 2: {solutions[1]}')
+    print("\nSolutions:")
+    print(f"\tPart 1: {solutions[0]}\n\tPart 2: {solutions[1]}")
