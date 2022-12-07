a8c4f0fdb433660202f9355ad98f634eb9931f0f
Noel Rajan <leonr13@hotmail.co.uk>
2022-12-07 07:43:21 +0000

    cleanup utils

--- utils.py
+++ utils.py
@@ -13,15 +13,15 @@ def get_input(day, year):
     test_input_dir = Path(f"./{year}/{day}/test_input.txt")
     print(f"Input Data: {puzzle_input_dir}")
 
-    puzzle_input = None
-    test_input = None
+    puzzle_input = ""
+    test_input = ""
     if puzzle_input_dir.is_file():
         puzzle_input = Path(puzzle_input_dir).read_text(encoding="utf-8")
-    
+
     if test_input_dir.is_file():
         test_input = Path(test_input_dir).read_text(encoding="utf-8")
     else:
-        test_input_dir.write_text(encoding="utf-8")
+        test_input_dir.write_text("", encoding="utf-8")
 
     if not puzzle_input:
         url = f"https://adventofcode.com/{year}/day/{day}/input"
